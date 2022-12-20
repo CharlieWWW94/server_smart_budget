99e2e990dbd3301edffeff2dc55c1631169b4fc5
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-12-20 11:41:00 +0000

    Add additional tests to models with rspec

--- spec/models/budget_item_spec.rb
+++ spec/models/budget_item_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe BudgetItem, type: :model do
     test_budget.destroy
     test_item.destroy
   end
+
+
+  it "is not valid when missing budget_id attribute" do
+    new_user = User.new({username: "test_user", pw_hash: "password", email: "testUser@email.com"})
+    new_user.save
+    test_budget = Budget.new({user_id: new_user.id})
+    test_budget.save
+    test_item = BudgetItem.new({value: 4000, name: "Rento", item_type: "variable"})
+    expect(test_item).to_not be_valid
+    new_user.destroy
+    test_budget.destroy
+    test_item.destroy
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,34 @@ RSpec.describe User, type: :model do
     new_user = User.new({username: "test_user", pw_hash: "password", email: "testUser@email.com"})
     expect(new_user).to be_valid  
   end
+  
   it "is not valid without username" do
   missing_username = User.new({username: nil, pw_hash: "password"})
   expect(missing_username).to_not be_valid
   end
+  
   it "is not valid without pw_hash" do
   missing_pw = User.new({username: "missing_pw"})
   expect(missing_pw).to_not be_valid
   end
 
   it "can access child (income)" do
+    new_user = User.new({username: "test_user", pw_hash: "password", email: "testUser@email.com"})
+    new_user.save
+    new_income = Income.new({income_type: "Fixed", annual: 20000, user_id: new_user.id})
+    new_income.save
+    expect(new_user.incomes[0][:annual]).to eq(20000)
+    new_user.destroy
+    new_income.destroy
   end
 
   it "can access child (budget)" do
+    new_user = User.new({username: "test_user", pw_hash: "password", email: "testUser@email.com"})
+    new_user.save
+    new_budget = Budget.new({title: "Test Budget", user_id: new_user.id})
+    new_budget.save
+    expect(new_user.budgets[0][:title]).to eq("Test Budget")
+    new_user.destroy
+    new_budget.destroy
   end
 end
